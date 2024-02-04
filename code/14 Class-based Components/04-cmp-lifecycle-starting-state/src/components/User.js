448da5941836c724566f4af51cdbcf3f0c67e577
kaitingp <kaiting.peck@gmail.com>
2024-02-04 14:19:39 +0800

    Commit after lifecycle methods in action

--- code/14 Class-based Components/04-cmp-lifecycle-starting-state/src/components/User.js
+++ code/14 Class-based Components/04-cmp-lifecycle-starting-state/src/components/User.js
@@ -3,9 +3,13 @@ import { Component } from 'react';
 import classes from './User.module.css';
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log("user will uncmount");
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
-  } 
+  }
 }
 
 // const User = (props) => {

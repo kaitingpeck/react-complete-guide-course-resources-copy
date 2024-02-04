3d6c2d9969c0861cb43be0ead53eab8850271a26
kaitingp <kaiting.peck@gmail.com>
2024-02-04 21:36:15 +0800

    Handle HTTP errors

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 // const places = localStorage.getItem('places'); // synchronous fetch
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   // creates an infinite loop - this code will be executed everytime component function executes - so a new request is sent every time the function executes
   // the state update for availablePlaces causes component function to execute -> fetch execute -> state update -> fetch execute -> ...
@@ -20,14 +22,31 @@ export default function AvailablePlaces({ onSelectPlace }) {
   useEffect(() => {
     setIsFetching(true);
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places

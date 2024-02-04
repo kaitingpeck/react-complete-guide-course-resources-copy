1b16814bd5040cdb79166dfd5a50e3665ac03c9e
kaitingp <kaiting.peck@gmail.com>
2024-02-04 21:21:34 +0800

    Use useEffect to fetch

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,35 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+
+// const places = localStorage.getItem('places'); // synchronous fetch
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // creates an infinite loop - this code will be executed everytime component function executes - so a new request is sent every time the function executes
+  // the state update for availablePlaces causes component function to execute -> fetch execute -> state update -> fetch execute -> ...
+  // fetch("http://localhost:3000/places")
+  // .then((response) => {
+  // return response.json();
+  // })
+  // .then((resData) => {
+  // setAvailablePlaces(resData.places);
+  // });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
